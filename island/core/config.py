5ffc91420d6fe0f807f9afeedd9e8b3a64e331cd
valid22 <10195318+valid22@users.noreply.github.com>
2023-06-15 14:12:17 +0530

    Upgrade base to V1.0.0 - Move from GINO to SQLAlchemy's async ORM + query-builder - World servers are separated into their own package - Support Kafka and Redis from external sources - Added sentry logging

--- island/core/config.py
+++ island/core/config.py
@@ -1,34 +1,68 @@
 import logging
 import sys
 from typing import List
-
 from loguru import logger
 from starlette.config import Config
 from starlette.datastructures import CommaSeparatedStrings, Secret, URL
-
-from island.core.logging import InterceptHandler  # pylint: disable=import-error
+from sqlalchemy.engine.url import URL, make_url
+from island.core.logging import InterceptHandler 
 
 config = Config(".env")
 
+# API config
 API_PREFIX = config("API_PREFIX", cast=str, default="")
 API_VERSION = config("API_VERSION", cast=str, default="0.0.1")
 API_SUFFIX_VERSION = config("API_SUFFIX_VERSION", cast=bool, default=True)
 if API_SUFFIX_VERSION:
     API_PREFIX = f"{API_PREFIX.strip('/')}/{API_VERSION.strip('/')}"
-DEBUG = config("DEBUG", cast=bool, default=False)
-SECRET_KEY: Secret = config(
-    "SECRET_KEY", cast=Secret, default="5df9db467ed2c905bcc1")
 ALLOWED_HOSTS: List[str] = config(
     "ALLOWED_HOSTS", cast=CommaSeparatedStrings, default=[]
 )
+
+# Logging config
+DEBUG = config("DEBUG", cast=bool, default=False)
 LOGGING_LEVEL = (
     logging.DEBUG
     if DEBUG
     else config("LOGGING_LEVEL", cast=lambda x: getattr(logging, x), default="INFO")
 )
+
+# Security config
+SECRET_KEY: Secret = config(
+    "SECRET_KEY", cast=Secret, default="5df9db467ed2c905bcc1")
 WORLD_ACCESS_KEY = config(
     "WORLD_ACCESS_KEY", cast=str, default="earlyDevelopmentTesting01"
 )
+
+# Database config
+DB_DRIVER = config("DB_DRIVER", default="postgresql+asyncpg")
+DB_HOST = config("DB_HOST", default="localhost")
+DB_PORT = config("DB_PORT", cast=int, default=None)
+DB_USER = config("DB_USER", default="postgres")
+DB_PASSWORD = config("DB_PASSWORD", cast=Secret, default="password")
+DB_DATABASE = config("DB_DATABASE", default="island")
+DB_DSN = config(
+    "DB_DSN",
+    cast=make_url,
+    default=URL.create(
+        drivername=DB_DRIVER,
+        username=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_DATABASE,
+    ),
+)
+
+
+# Sentry config
+SENTRY_DSN = config("SENTRY_DSN", cast=Secret, default="")
+
+# Redis config
+REDIS_HOST = config("REDIS_HOST", cast=str, default="127.0.0.1")
+REDIS_PORT = config("REDIS_PORT", cast=int, default=6379)
+REDIS_PASSWORD = config("REDIS_PASSWORD", cast=Secret, default=None)
+REDIS_SSL_REQUIRED = config("REDIS_SSL_REQUIRED", cast=bool, default=True)
 
 logging.getLogger().handlers = [InterceptHandler()]
 LOGGERS = ("uvicorn.asgi", "uvicorn.access")
--- island/database/__init__.py
+++ island/database/__init__.py
@@ -1,29 +1,13 @@
-from databases import Database
-from sqlalchemy.engine.url import URL, make_url
-from starlette.datastructures import Secret
-from gino import Gino
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+from island.core.config import DB_DSN
 
-from island.core.config import config
-
-DB_DRIVER = config("DB_DRIVER", default="postgresql")
-DB_HOST = config("DB_HOST", default="localhost")
-DB_PORT = config("DB_PORT", cast=int, default=None)
-DB_USER = config("DB_USER", default="postgres")
-DB_PASSWORD = config("DB_PASSWORD", cast=Secret, default="password")
-DB_DATABASE = config("DB_DATABASE", default="island")
-DB_DSN = config(
-    "DB_DSN",
-    cast=make_url,
-    default=URL(
-        drivername=DB_DRIVER,
-        username=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        database=DB_DATABASE,
-    ),
+Base = declarative_base()
+ASYNC_ENGINE = create_async_engine(DB_DSN, echo=True)
+ASYNC_SESSION = async_sessionmaker(
+    ASYNC_ENGINE, expire_on_commit=False
 )
 
-
-db = Gino()
-Base = db.Model
+async def create_all(self):
+    async with ASYNC_ENGINE.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
