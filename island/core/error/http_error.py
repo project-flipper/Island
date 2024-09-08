eca4580a7d061a5378558e0e09bda6ed5fcb0c36
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-08 02:27:59 -0600

    Change imports from starlette

--- island/core/error/http_error.py
+++ island/core/error/http_error.py
@@ -1,8 +1,8 @@
 from starlette.exceptions import HTTPException
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from starlette.requests import Request
-from starlette.responses import JSONResponse
+from fastapi import Request
+from fastapi.responses import JSONResponse
 
 
 async def http_error_handler(_: Request, exc: Exception) -> JSONResponse:
--- island/core/error/validation_error.py
+++ island/core/error/validation_error.py
@@ -2,8 +2,8 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.openapi.constants import REF_PREFIX
 from fastapi.openapi.utils import validation_error_response_definition
 from pydantic import ValidationError
-from starlette.requests import Request
-from starlette.responses import JSONResponse
+from fastapi import Request
+from fastapi.responses import JSONResponse
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 
