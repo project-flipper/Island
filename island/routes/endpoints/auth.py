9ee69a588e029a81f1ba3835337f7a5914d5834f
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-08 02:28:48 -0600

    Change default scopes

--- island/routes/endpoints/auth.py
+++ island/routes/endpoints/auth.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, Request, Response, status
 from fastapi.param_functions import Form
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
-from starlette.responses import JSONResponse
+from fastapi.responses import JSONResponse
 
 from island.core.config import config
 from island.core.constants.scope import Scope
@@ -35,7 +36,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = config(
 @router.post("/login")
 async def handle_authenticate_user(
     response: Response,
-    auth_input: OAuth2PasswordRequestForm = Depends(),
+    auth_input: Annotated[OAuth2PasswordRequestForm, Depends()],
     save_session: bool = Form(
         False,
         description="Returns an access key which can be further used to reauthenticate without need for username and password, if set to True",
@@ -65,7 +66,7 @@ async def handle_authenticate_user(
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return TokenResponse(
             error=Error(
-                error_type="user.auth.failed",
+                error_type="user.auth.credentials",
                 error_code=101,
                 error_description="User authentication failed. Incorrect username or password.",
             ),
@@ -88,7 +89,7 @@ async def handle_authenticate_user(
         )
 
     access_token_expires = timedelta(seconds=ACCESS_TOKEN_EXPIRE_MINUTES)
-    user_scopes = await get_user_scopes(user, default_scopes=[Scope.UserLogin])
+    user_scopes = await get_user_scopes(user, default_scopes=[Scope.UserLogin, Scope.WorldAccess])
 
     access_token = create_access_token(
         data={
@@ -118,7 +119,7 @@ async def handle_authenticate_user(
     dependencies=[require_oauth_scopes(Scope.UserAuth)],
 )
 async def handle_reauthenticate_user(
-    response: Response, user: UserTable = Depends(get_current_user)
+    response: Response, user: Annotated[UserTable, Depends(get_current_user)]
 ) -> TokenResponse:
     user_ban = user.bans[0] if user.bans else None
     if user_ban is not None:
@@ -135,7 +136,7 @@ async def handle_reauthenticate_user(
         )
 
     access_token_expires = timedelta(seconds=ACCESS_TOKEN_EXPIRE_MINUTES)
-    user_scopes = await get_user_scopes(user, default_scopes=[Scope.UserLogin])
+    user_scopes = await get_user_scopes(user, default_scopes=[Scope.UserLogin, Scope.WorldAccess])
 
     access_token = create_access_token(
         data={
