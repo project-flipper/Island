a24b9e14f64aa28715f9f1f897a8d19cc6103d0a
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-08 14:07:20 -0600

    Allow conversion between model and DB OEM

--- island/models/user.py
+++ island/models/user.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from island.database import ASYNC_SESSION
+from island.database.schema.user import UserTable
 from island.models.avatar import Avatar
 from island.models.presence import Presence
 from island.models.relationship import Relationship
@@ -19,7 +21,42 @@ class User(BaseUser):
     publicStampbook: bool
     presence: Presence | None
 
+    @classmethod
+    async def from_user_table(cls, user: UserTable) -> "User":
+        async with ASYNC_SESSION() as session:
+            user = await session.merge(user)
+
+            return User(
+                id=str(user.id),
+                username=user.username,
+                nickname=user.nickname,
+                avatar=Avatar.model_validate(user.avatar, from_attributes=True),
+                member=None,
+                iglooId=0,
+                mascotId=None,
+                relationship=None,
+                publicStampbook=False,
+                presence=None
+            )
+
 class MyUser(BaseUser):
     moderator: bool
     iglooId: int
     stealth: bool
+
+    @classmethod
+    async def from_user_table(cls, user: UserTable) -> "MyUser":
+        async with ASYNC_SESSION() as session:
+            user = await session.merge(user)
+
+            return MyUser(
+                id=str(user.id),
+                username=user.username,
+                nickname=user.nickname,
+                avatar=Avatar.model_validate(user.avatar, from_attributes=True),
+                member=None,
+                iglooId=0,
+                mascotId=None,
+                moderator=True,
+                stealth=False
+            )
