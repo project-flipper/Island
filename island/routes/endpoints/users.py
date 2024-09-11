56b5d0a8b3c0bcf49067bf3f52cfb3aeeddbee6e
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-10 22:55:13 -0600

    Reorder usesr endpoint

--- island/routes/endpoints/users.py
+++ island/routes/endpoints/users.py
@@ -86,6 +86,33 @@ async def get_my_user(user: Annotated[UserTable, Depends(get_current_user)]) -> 
     my_user = await MyUser.from_orm(user)
     return Response(data=my_user, success=True)
 
+@router.get("/search", dependencies=[require_oauth_scopes()])
+async def get_user_by_name(username: str, my_user_id: Annotated[int, Depends(get_current_user_id)]) -> Response[User | MyUser]:
+    async with ASYNC_SESSION() as session:
+        user_query = (
+            select(UserTable)
+            .where(func.lower(UserTable.nickname) == username.lower())
+        )
+
+        user = (await session.execute(user_query)).scalar()
+
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=Error(
+                    error_type="users.query.not_found",
+                    error_code=404,
+                    error_description="User not found",
+                ),
+            )
+
+        if user.id == my_user_id:
+            user_model = await MyUser.from_orm(user)
+        else:
+            user_model = await User.from_orm(user)
+
+        return Response(data=user_model, success=True)
+
 @router.get("/{user_id}", dependencies=[require_oauth_scopes()])
 async def get_user_by_id(user_id: int, my_user_id: Annotated[int, Depends(get_current_user_id)]) -> Response[User | MyUser]:
     if user_id == my_user_id:
@@ -122,30 +149,3 @@ async def get_user_by_id(user_id: int, my_user_id: Annotated[int, Depends(get_cu
 
             user_model = await User.from_orm(user)
             return Response(data=user_model, success=True)
-
-@router.get("/search", dependencies=[require_oauth_scopes()])
-async def get_user_by_name(username: str, my_user_id: Annotated[int, Depends(get_current_user_id)]) -> Response[User | MyUser]:
-    async with ASYNC_SESSION() as session:
-        user_query = (
-            select(UserTable)
-            .where(func.lower(UserTable.nickname) == username.lower())
-        )
-
-        user = (await session.execute(user_query)).scalar()
-
-        if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=Error(
-                    error_type="users.query.not_found",
-                    error_code=404,
-                    error_description="User not found",
-                ),
-            )
-
-        if user.id == my_user_id:
-            user_model = await MyUser.from_orm(user)
-        else:
-            user_model = await User.from_orm(user)
-
-        return Response(data=user_model, success=True)
