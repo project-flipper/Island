c3ecdab87cf34a36da3c9b53ee02348c25c2135f
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-14 21:45:45 -0600

    Merge if

--- island/utils/routes.py
+++ island/utils/routes.py
@@ -16,9 +16,8 @@ def get_modules(module: ModuleType, /, *, global_namespace: str):
     if module_path.endswith("__init__.py"):
         module_path = module_path.split("__init__.py")[0]
 
-    if global_namespace is not None:
-        if not hasattr(builtins, global_namespace):
-            setattr(builtins, global_namespace, [])
+    if global_namespace is not None and not hasattr(builtins, global_namespace):
+        setattr(builtins, global_namespace, [])
 
     for a, module_name, is_pkg in iter_modules([module_path]):
         m_name = f"{module.__name__}.{module_name}"
