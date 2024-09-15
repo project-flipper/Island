857f737b3626ad608c09e69daf1a41766ccd8d0e
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-14 21:37:36 -0600

    Define __all__ in schema

--- island/database/schema/__init__.py
+++ island/database/schema/__init__.py
@@ -2,3 +2,5 @@ from .avatar import AvatarTable
 from .ban import BanTable
 from .user import UserTable
 from .world import WorldTable
+
+__all__ = ('AvatarTable', 'BanTable', 'UserTable', 'WorldTable')
