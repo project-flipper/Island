0c354d8ebf1d2fecce8c00136a471f05d4e222a3
DanLCD <44991848+DanLCD@users.noreply.github.com>
2024-09-29 12:58:19 -0600

    Split world connect event

--- island/core/constants/events.py
+++ island/core/constants/events.py
@@ -5,4 +5,5 @@ class EventEnum(Enum):
     APP_START_EVENT = "app:start"
     APP_STOP_EVENT = "app:stop"
     WORLD_CLIENT_CONNECT = "world:client:connect"
+    WORLD_CLIENT_AUTH = "world:client:auth"
     WORLD_CLIENT_DISCONNECT = "world:client:disconnect"
