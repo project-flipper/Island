ddf54819ac86e2454ea5c9a4469e4a6b58a68c68
Lekuru <contact@lekuru.xyz>
2024-10-06 21:15:18 +0200

    Add `puffles` & `puffle_items` table

--- island/database/schema/__init__.py
+++ island/database/schema/__init__.py
@@ -1,6 +1,7 @@
 from .igloo import IglooTable, IglooFloorTable, IglooLocationTable
 from .polaroids import PolaroidTable, PolaroidCollectionTable
 from .postcards import PostcardTable, PostcardCollectionTable
+from .puffles import PuffleTable, PuffleItemTable
 from .game_strings import GameStringsTable
 from .newspapers import NewspaperTable
 from .furniture import FurnitureTable
@@ -34,4 +35,6 @@ __all__ = (
     'PolaroidCollectionTable',
     'PostcardTable',
     'PostcardCollectionTable',
+    'PuffleTable',
+    'PuffleItemTable',
 )
---
+++ island/database/schema/puffles.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from sqlalchemy import String, SmallInteger, Integer, ForeignKey, ARRAY
+from sqlalchemy.orm import Mapped, mapped_column
+from island.database import Base
+
+class PuffleTable(Base):
+    __tablename__ = "puffles"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    parent_id: Mapped[int] = mapped_column(Integer, ForeignKey("puffles.id"))
+    description: Mapped[str] = mapped_column(String(256))
+    color: Mapped[int] = mapped_column(Integer)
+    member: Mapped[bool] = mapped_column(default=False)
+    language: Mapped[int] = mapped_column(SmallInteger, primary_key=True)
+
+class PuffleItemTable(Base):
+    __tablename__ = "puffle_items"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(256))
+    path: Mapped[str] = mapped_column(String(256))
+    cost: Mapped[int] = mapped_column(Integer, default=0)
+    quantity: Mapped[int] = mapped_column(Integer, default=1)
+    member: Mapped[bool] = mapped_column(default=False)
+    play_external: Mapped[str] = mapped_column(String(256))
+    consumption: Mapped[str] = mapped_column(String(256))
+    class_name: Mapped[str] = mapped_column(String(256))
+    root_item_id: Mapped[int] = mapped_column(Integer)
+    only_purchase: Mapped[bool] = mapped_column(default=False)
+    effect_food: Mapped[int] = mapped_column(SmallInteger)
+    effect_rest: Mapped[int] = mapped_column(SmallInteger)
+    effect_play: Mapped[int] = mapped_column(SmallInteger)
+    effect_clean: Mapped[int] = mapped_column(SmallInteger)
+    reaction: Mapped[list] = mapped_column(ARRAY(Integer))
+    language: Mapped[int] = mapped_column(SmallInteger, primary_key=True)
