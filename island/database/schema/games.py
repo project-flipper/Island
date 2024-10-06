a5131d918a2205ec4303c6dee607818891de7f46
Lekuru <contact@lekuru.xyz>
2024-10-06 19:49:15 +0200

    Add `games` table

---
+++ island/database/schema/games.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
+from island.database import Base
+
+class GameTable(Base):
+    __tablename__ = "games"
+
+    key: Mapped[str] = mapped_column(String(256), primary_key=True)
+    name: Mapped[str] = mapped_column(String(256))
+    path: Mapped[str] = mapped_column(String(256))
+    room_id: Mapped[int] = mapped_column(Integer)
+    music_id: Mapped[int] = mapped_column(Integer)
+    stamp_set_id: Mapped[int] = mapped_column(Integer)
+    show_player_in_room: Mapped[bool] = mapped_column(default=False)
