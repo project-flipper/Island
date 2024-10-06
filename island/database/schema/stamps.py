0d94aa78b682318e1e6c45348c8a3be95f9cde04
Lekuru <contact@lekuru.xyz>
2024-10-07 00:02:33 +0200

    Use `StampRank` enum in sqlalchemy

--- island/database/schema/stamps.py
+++ island/database/schema/stamps.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, ForeignKey, SmallInteger
+from island.core.constants.stamps import StampRank
+from sqlalchemy import String, ForeignKey, Enum
 from island.database import Base
 
 
@@ -12,7 +13,7 @@ class StampTable(Base):
     collection_id: Mapped[int] = mapped_column(ForeignKey("stamps_collection.id"))
     name: Mapped[str] = mapped_column(String(256))
     member: Mapped[bool] = mapped_column(default=False)
-    rank: Mapped[int] = mapped_column(SmallInteger)
+    rank: Mapped[StampRank] = mapped_column(Enum(StampRank))
     description: Mapped[str] = mapped_column(String(256))
 
     collection: Mapped["StampCollectionTable"] = relationship(
