5cf6fee2fef9fdd3b5a7e1d05261143039497ca8
Lekuru <contact@lekuru.xyz>
2024-10-06 19:49:06 +0200

    Add `game_strings` table

---
+++ island/database/schema/game_strings.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from sqlalchemy import String, Text, SmallInteger
+from sqlalchemy.orm import Mapped, mapped_column
+from island.database import Base
+
+class GameStringsTable(Base):
+    __tablename__ = "game_strings"
+
+    key: Mapped[str] = mapped_column(Text, primary_key=True)
+    value: Mapped[str] = mapped_column(Text)
+    category: Mapped[str] = mapped_column(String(256))
