b1cd6108bd11332596d824a07fd4c33852081cc1
Lekuru <contact@lekuru.xyz>
2024-10-06 22:42:07 +0200

    Add `user_id` reference to mascot table

--- island/database/schema/mascots.py
+++ island/database/schema/mascots.py
@@ -7,14 +7,17 @@ from island.database import Base
 
 if TYPE_CHECKING:
     from island.database.schema.items import ItemTable
+    from island.database.schema.user import UserTable
 
 
 class MascotTable(Base):
     __tablename__ = "mascots"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     name: Mapped[str] = mapped_column(String(256))
     title: Mapped[str] = mapped_column(String(256))
     gift_id: Mapped[int] = mapped_column(Integer, ForeignKey("items.id"))
 
     gift: Mapped[ItemTable] = relationship("ItemTable")
+    user: Mapped[UserTable] = relationship("UserTable")
