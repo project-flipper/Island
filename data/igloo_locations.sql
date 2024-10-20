dffca2542c146fb6bc5a679507fcb41e3f08491e
Lekuru <contact@lekuru.xyz>
2024-10-20 17:59:21 +0200

    Add sql files for data migrations

---
+++ data/igloo_locations.sql
@@ -0,0 +1,9 @@
+INSERT INTO igloo_locations (id, igloo_id, name, cost) VALUES
+ (1, 45, 'Default', 0),
+ (2, 46, 'Beach', 2800),
+ (3, 47, 'Forest', 2700),
+ (4, 48, 'Mountain', 2800),
+ (5, 59, 'Age Of Dinosaurs', 0),
+ (6, 72, 'Distant Planet', 1800),
+ (7, 74, 'Soccer Pitch', 1800),
+ (8, 80, 'Undersea', 1800);
---
+++ data/options.sql
@@ -0,0 +1,12 @@
+INSERT INTO game_options (key, value) VALUES
+ ('mascot_options.migrator_active', 'False'),
+ ('party_options.fair_ticket_active', 'False'),
+ ('party_options.hunt_active', 'False'),
+ ('party_options.itemRewardID', '1388'),
+ ('party_options.isMapNoteActive', 'False'),
+ ('party_options.showPartyAnnouncement', 'False'),
+ ('party_options.party_icon_active', 'True'),
+ ('igloo_options.contestRunning', 'False'),
+ ('oops_test.testEnabled', 'True'),
+ ('island_options.isDaytime', 'True'),
+ ('party_dates.20170201', '2017-01-30');
