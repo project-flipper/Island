64d97f0e83822cc518687b7b16ba282cbcc041fa
Lekuru <contact@lekuru.xyz>
2024-10-21 22:25:56 +0200

    Remove unused tables

--- island/database/schema/__init__.py
+++ island/database/schema/__init__.py
@@ -1,17 +1,14 @@
 from .igloo import IglooTable, IglooFloorTable, IglooLocationTable
-from .polaroids import PolaroidTable, PolaroidCollectionTable
-from .stamps import StampTable, StampCollectionTable
 from .puffles import PuffleTable, PuffleItemTable
 from .furniture import FurnitureTable
 from .postcards import PostcardTable
-from .tour import TourMessagesTable
 from .game import GameOptionsTable
 from .mascots import MascotTable
 from .avatar import AvatarTable
+from .stamps import StampTable
 from .world import WorldTable
 from .games import GameTable
 from .items import ItemTable
-from .jokes import JokeTable
 from .rooms import RoomTable
 from .card import CardTable
 from .user import UserTable
@@ -29,16 +26,11 @@ __all__ = (
     "IglooTable",
     "IglooFloorTable",
     "IglooLocationTable",
-    "JokeTable",
     "ItemTable",
     "MascotTable",
-    "PolaroidTable",
-    "PolaroidCollectionTable",
     "PostcardTable",
     "PuffleTable",
     "PuffleItemTable",
     "RoomTable",
     "StampTable",
-    "StampCollectionTable",
-    "TourMessagesTable",
 )
